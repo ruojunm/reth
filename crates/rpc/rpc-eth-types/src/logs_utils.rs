79882fae82760ebf455e3ca8cf663eba09ec8103
VM <arimas@foxmail.com>
2025-09-16 10:46:17 +0800

    fix: add block timestamp field

--- crates/rpc/rpc-eth-types/src/logs_utils.rs
+++ crates/rpc/rpc-eth-types/src/logs_utils.rs
@@ -73,7 +73,7 @@ pub fn append_matching_block_logs<P>(
     block_num_hash: BlockNumHash,
     receipts: &[P::Receipt],
     removed: bool,
-    _block_timestamp: u64,
+    block_timestamp: u64,
 ) -> Result<(), ProviderError>
 where
     P: BlockReader<Transaction: SignedTransaction>,
@@ -135,7 +135,7 @@ where
                     transaction_index: Some(receipt_idx as u64),
                     log_index: Some(log_index),
                     removed,
-                    block_timestamp: None,
+                    block_timestamp: Some(block_timestamp),
                 };
                 all_logs.push(log);
             }
--- crates/rpc/rpc-eth-types/src/receipt.rs
+++ crates/rpc/rpc-eth-types/src/receipt.rs
@@ -32,16 +32,12 @@ where
     let cumulative_gas_used = receipt.cumulative_gas_used();
     let logs_bloom = receipt.bloom();
 
-    let mut logs = match receipt {
+    let logs = match receipt {
         Cow::Borrowed(r) => {
             Log::collect_for_receipt(*next_log_index, *meta, r.logs().iter().cloned())
         }
         Cow::Owned(r) => Log::collect_for_receipt(*next_log_index, *meta, r.into_logs()),
     };
-
-    for log in &mut logs {
-        log.block_timestamp = None;
-    }
 
     let rpc_receipt = alloy_rpc_types_eth::Receipt { status, cumulative_gas_used, logs };
 
